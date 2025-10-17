d339c97d0e33822b190864ed3c5b0d2c625cbdaa
Nikitenko Kirill <kirillnk89nam@gmail.com>
2025-10-17 14:32:15 +0700

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,78 +1,3 @@
-# from django.http import JsonResponse
-# from .models import Transaction, Category, SubCategory
-# from django.shortcuts import render, redirect, get_object_or_404
-# from .models import Transaction
-# from .forms import TransactionForm
-
-
-# def transaction_list(request):
-#     transactions = Transaction.objects.all()
-#     context = {
-#         'transactions': transactions
-#     }
-#     return render(request, 'core/transaction_list.html', context)
-
-
-# def transaction_create(request):
-#     if request.method == 'POST':
-#         form = TransactionForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('transaction_list')
-#     else:
-#         form = TransactionForm()
-
-#     context = {
-#         'form': form
-#     }
-#     return render(request, 'core/transaction_form.html', context)
-
-
-# def transaction_update(request, pk):
-#     transaction = get_object_or_404(Transaction, pk=pk)
-#     if request.method == 'POST':
-#         form = TransactionForm(request.POST, instance=transaction)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('transaction_list')
-#     else:
-#         form = TransactionForm(instance=transaction)
-
-#     context = {
-#         'form': form
-#     }
-#     return render(request, 'core/transaction_form.html', context)
-
-
-# def transaction_delete(request, pk):
-#     transaction = get_object_or_404(Transaction, pk=pk)
-#     if request.method == 'POST':
-#         transaction.delete()
-#         return redirect('transaction_list')
-
-#     context = {
-#         'transaction': transaction
-#     }
-#     return render(request, 'core/transaction_confirm_delete.html', context)
-
-# # NEW VIEW 1: Load categories based on a type_id
-
-
-# def load_categories(request):
-#     type_id = request.GET.get('type_id')
-#     categories = Category.objects.filter(type_id=type_id).order_by('name')
-#     # We return the data in a format that's easy for JavaScript to use
-#     return JsonResponse(list(categories.values('id', 'name')), safe=False)
-
-# # NEW VIEW 2: Load subcategories based on a category_id
-
-
-# def load_subcategories(request):
-#     category_id = request.GET.get('category_id')
-#     subcategories = SubCategory.objects.filter(
-#         category_id=category_id).order_by('name')
-#     return JsonResponse(list(subcategories.values('id', 'name')), safe=False)
-
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from .models import Transaction, Status, Type, Category, SubCategory
@@ -87,10 +12,6 @@ def transaction_list(request):
     types = Type.objects.all()
     categories = Category.objects.all()
 
-    # --- THIS IS THE NEW SIMPLER LOGIC ---
-
-    # We will just pass the raw request.GET object to the template
-    # and let the template handle the logic.
 
     if request.GET.get('status'):
         transactions = transactions.filter(status_id=request.GET.get('status'))
@@ -118,8 +39,6 @@ def transaction_list(request):
         'filters': request.GET  # We pass the raw request.GET data
     }
     return render(request, 'core/transaction_list.html', context)
-
-# --- THE REST OF OUR VIEWS ARE UNCHANGED ---
 
 
 def transaction_create(request):
@@ -161,8 +80,6 @@ def transaction_delete(request, pk):
     }
     return render(request, 'core/transaction_confirm_delete.html', context)
 
-
-# --- OUR AJAX VIEWS ARE ALSO UNCHANGED ---
 
 def load_categories(request):
     type_id = request.GET.get('type_id')
