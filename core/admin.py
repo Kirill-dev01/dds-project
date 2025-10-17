aa49d01788755ca785b78dbaf310185f0e811c18
Nikitenko Kirill <kirillnk89nam@gmail.com>
2025-10-17 14:30:28 +0700

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Status, Type, Category, SubCategory, Transaction
 
-# This tells Django to create an admin interface for each of your models.
 admin.site.register(Status)
 admin.site.register(Type)
 admin.site.register(Category)
